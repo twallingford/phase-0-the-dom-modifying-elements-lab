9e98e42eeadc33dfccf5879661b33643d22d3712
Thomas Wallingford <tbwallingford@gmail.com>
2023-03-19 09:07:08 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Thomas is the champion";
+document.body.append(newHeader);
